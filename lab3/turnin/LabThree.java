3ce56a221e509b0e000853b6ceb2be86fe7c0033
Surya Dantuluri <surya.mvhs@gmail.com>
2019-02-09 23:40:52 -0800

    udpate

--- lab3/turnin/LabThree.java
+++ lab3/turnin/LabThree.java
@@ -13,3 +13,182 @@ public class LabThree {
         System.out.println(x);
     }
 }
+
+class DoublyLinkedList <T>
+{
+	public Node head;
+	public Node tail;
+
+	public DoublyLinkedList()
+	{
+		head = null;
+		tail = null;
+	}
+
+	/** Inserts Node newNode after Node curNode
+	*  @param curNode Node previous to inserted node
+	*  @param newNode Node that is inserted after curNode
+	*/
+	public void insert(Node curNode, Node newNode)
+	{
+		//Temporary Node
+		Node sucNode;
+
+		// List empty
+		if (head == null) 
+		{ 
+			head = newNode;
+			tail = newNode;
+		}
+		// Insert after tail
+		else if (curNode == tail) 
+		{ 
+			tail.setNext(newNode);
+			newNode.setPrev(tail);
+			tail = newNode;
+		}
+		else
+		{
+			sucNode = curNode.next;
+			newNode.setNext(sucNode);
+			newNode.setPrev(curNode);
+			curNode.setNext(newNode);
+			sucNode.setPrev(newNode);
+		}
+	}
+
+	/** Deletes Node after Node curNode.
+	*  @param curNode Node previous to deleted node
+	*/
+	public void delete(Node curNode)
+	{
+		//Temporary Node
+		Node sucNode;
+		Node predNode;
+		sucNode = curNode.next;
+		predNode = curNode.prev;
+
+		if (sucNode!=null) 
+		{
+			sucNode.prev = predNode;
+			sucNode.setPrev(predNode);
+		}
+
+		if (predNode!=null) 
+		{
+			predNode.setNext(sucNode);
+		}
+
+		// Removed head
+		if (curNode == head) 
+		{ 
+			head = sucNode;
+		}
+
+		// Removed tail
+		if (curNode == tail) 
+		{ 
+			tail = predNode;
+		}
+	}
+
+	// Inserts node at the start of the list
+	public void prepend(Node curNode)
+	{
+		curNode.setNext(head);
+		curNode.setPrev(null);
+
+		if(head != null)
+			head.setPrev(curNode);
+
+		head = curNode;
+
+	}
+
+	// Inserts node at the end of the list
+	public void append(Node curNode)
+	{
+
+		Node traverseNode = head;
+
+		curNode.setNext(null);
+
+		if(head==null)
+		{
+			curNode.setPrev(null);
+			head = curNode;
+			return;
+		}
+
+		while(traverseNode.next!=null)
+			traverseNode = traverseNode.next;
+
+		traverseNode.setNext(curNode);
+		curNode.setPrev(traverseNode);  
+	}
+
+	// Returns head of Doubly linked list
+	public Node getHead()
+	{
+		return head;
+	}
+
+	// Returns tail of Doubly linked list
+	public Node getTail()
+	{
+		return tail;
+	}
+
+	/** Prints Doubly Linked List
+	*/
+	public void printList()
+	{
+		Node temp = head; // start at the head node
+		while (temp != null)
+		{
+			System.out.print(temp.get() + " ");
+			temp = temp.next; // go to next node
+		}
+		System.out.println();
+		System.out.println();
+	}
+
+	public int size()
+	{
+		int sizeCount = 0;
+		Node temp = head; // start at the first node
+		while (temp != null)
+		{
+			sizeCount++;
+			temp = temp.next; // go to next node
+		}
+		return sizeCount;
+	}
+
+}
+class Customer
+{
+	public String firstname;
+	public String lastname;
+    public int keyId;
+    
+	public Customer(String fname, String lname, int lid)
+	{
+		this.firstname = fname;
+		this.lastname = lname;
+		this.keyId = lid;
+	}
+
+	public String getFirstName()
+	{
+		return firstname;
+	}
+	public String getLastName()
+	{
+		return lastname;
+	}
+	public int getId()
+	{
+		return keyId;
+	}
+}
