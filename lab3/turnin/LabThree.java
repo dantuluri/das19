c4f03f0182fd1c29152d9de217f3b1275d8b8a47
Surya Dantuluri <surya.mvhs@gmail.com>
2019-02-09 21:27:23 -0800

    add

---
+++ lab3/turnin/LabThree.java
@@ -0,0 +1,15 @@
+public class LabThree {
+    public static void main(String[] args) {
+        LabThree three = new LabThree();
+        three.run();
+    }
+
+    public void run() {
+        extracted();
+    }
+
+    private void extracted() {
+        String x = " orld";
+        System.out.println(x);
+    }
+}
